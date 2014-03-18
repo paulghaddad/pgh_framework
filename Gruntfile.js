7c1db3370fdcbfde76fcea588623305fdd760da9
Paul Haddad <paulh16@gmail.com>
2014-03-18 10:09:49 +0000

    Adjust Gruntfile and img directory for imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,9 +20,12 @@ module.exports = function(grunt) {
 
     imagemin: {                          // Task
       dynamic: {                         // Another target
+        options: {
+          cache: false
+        },
         files: [{
           expand: true,                  // Enable dynamic expansion
-          cwd: 'img/',                   // Src matches are relative to this path
+          cwd: 'img/prebuild',                   // Src matches are relative to this path
           src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
           dest: 'img/build/'                  // Destination path prefix
         }]
